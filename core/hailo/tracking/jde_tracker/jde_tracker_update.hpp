cc23922c17737d708180516429489e7c4a998fd2
PhaniOEPL <phanisandeep.ch@optimizedelectrotech.com>
2025-05-21 12:38:26 +0530

    Update jde_tracker_update.hpp removed update_trackmode

--- core/hailo/tracking/jde_tracker/jde_tracker_update.hpp
+++ core/hailo/tracking/jde_tracker/jde_tracker_update.hpp
@@ -171,63 +171,63 @@ inline void JDETracker::update_unmatches(std::vector<STrack *> strack_pool,
     }
 }
 
-inline void JDETracker::update_trackmode(std::vector<STrack> &stracksa,std::vector<STrack> &stracksb,std::vector<STrack> &stracksc)
-{
-	if(m_track_shmp->_selectedTarget!=-1)//if not mot
-	{
-		//search in tracked tracks
-		for (uint i = 0; i < stracksa.size(); i++)
-    		{
-        		if(stracksa[i].m_track_id==m_track_shmp->_selectedTarget)
-			{
-				STrack sot_track=stracksa[i];
+// inline void JDETracker::update_trackmode(std::vector<STrack> &stracksa,std::vector<STrack> &stracksb,std::vector<STrack> &stracksc)
+// {
+// 	if(m_track_shmp->_selectedTarget!=-1)//if not mot
+// 	{
+// 		//search in tracked tracks
+// 		for (uint i = 0; i < stracksa.size(); i++)
+//     		{
+//         		if(stracksa[i].m_track_id==m_track_shmp->_selectedTarget)
+// 			{
+// 				STrack sot_track=stracksa[i];
 
-                std::vector<float> xyah= sot_track.to_xyah();
-                m_track_shmp->_sot_track.cX        = xyah[0]*m_track_shmp->_model_input_size_x;
-                m_track_shmp->_sot_track.cY        = xyah[1]*m_track_shmp->_model_input_size_y;
-				m_track_shmp->_sot_track.width     = xyah[2]*xyah[3]*m_track_shmp->_model_input_size_x; //a*h
-				m_track_shmp->_sot_track.height    = xyah[3]*m_track_shmp->_model_input_size_y; //h
+//                 std::vector<float> xyah= sot_track.to_xyah();
+//                 m_track_shmp->_sot_track.cX        = xyah[0]*m_track_shmp->_model_input_size_x;
+//                 m_track_shmp->_sot_track.cY        = xyah[1]*m_track_shmp->_model_input_size_y;
+// 				m_track_shmp->_sot_track.width     = xyah[2]*xyah[3]*m_track_shmp->_model_input_size_x; //a*h
+// 				m_track_shmp->_sot_track.height    = xyah[3]*m_track_shmp->_model_input_size_y; //h
 
-                m_track_shmp->_sot_track.trackID   = sot_track.m_track_id;
-				m_track_shmp->_sot_track.classtype = sot_track.m_class_id;
+//                 m_track_shmp->_sot_track.trackID   = sot_track.m_track_id;
+// 				m_track_shmp->_sot_track.classtype = sot_track.m_class_id;
 				
-                m_track_shmp->_bValidTrack=true;
+//                 m_track_shmp->_bValidTrack=true;
 
 				
-				return;
-			}
-		}
+// 				return;
+// 			}
+// 		}
 		
-		//search in lost tracks
-		for (uint i = 0; i < stracksb.size(); i++)
-    		{
-        		if(stracksb[i].m_track_id==m_track_shmp->_selectedTarget)
-			{
-				STrack sot_track=stracksb[i];
+// 		//search in lost tracks
+// 		for (uint i = 0; i < stracksb.size(); i++)
+//     		{
+//         		if(stracksb[i].m_track_id==m_track_shmp->_selectedTarget)
+// 			{
+// 				STrack sot_track=stracksb[i];
 
-                std::vector<float> xyah= sot_track.to_xyah();
-				m_track_shmp->_sot_track.cX        = xyah[0]*m_track_shmp->_model_input_size_x;
-        		m_track_shmp->_sot_track.cY        = xyah[1]*m_track_shmp->_model_input_size_y;
-				m_track_shmp->_sot_track.width     = xyah[2]*xyah[3]*m_track_shmp->_model_input_size_x; //a*h
-				m_track_shmp->_sot_track.height    = xyah[3]*m_track_shmp->_model_input_size_y; //h
-        		m_track_shmp->_sot_track.trackID   = sot_track.m_track_id;
+//                 std::vector<float> xyah= sot_track.to_xyah();
+// 				m_track_shmp->_sot_track.cX        = xyah[0]*m_track_shmp->_model_input_size_x;
+//         		m_track_shmp->_sot_track.cY        = xyah[1]*m_track_shmp->_model_input_size_y;
+// 				m_track_shmp->_sot_track.width     = xyah[2]*xyah[3]*m_track_shmp->_model_input_size_x; //a*h
+// 				m_track_shmp->_sot_track.height    = xyah[3]*m_track_shmp->_model_input_size_y; //h
+//         		m_track_shmp->_sot_track.trackID   = sot_track.m_track_id;
 
-				m_track_shmp->_sot_track.classtype = sot_track.m_class_id;
-        		m_track_shmp->_bValidTrack=true;
+// 				m_track_shmp->_sot_track.classtype = sot_track.m_class_id;
+//         		m_track_shmp->_bValidTrack=true;
 
-				return;
-			}
-		}
+// 				return;
+// 			}
+// 		}
 
-		m_track_shmp->_bValidTrack=false; //Note:: so track is lost permanently -couldnt find the sot either in active or in lost tracks
+// 		m_track_shmp->_bValidTrack=false; //Note:: so track is lost permanently -couldnt find the sot either in active or in lost tracks
 
-	}
-	else
-	{
-        	m_track_shmp->_bValidTrack=false;
-	}
-	return;
-}
+// 	}
+// 	else
+// 	{
+//         	m_track_shmp->_bValidTrack=false;
+// 	}
+// 	return;
+// }
 
 
 
