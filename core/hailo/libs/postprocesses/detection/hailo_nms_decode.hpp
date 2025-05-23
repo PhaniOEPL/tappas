f1888aede11c91385009ebfe737d90216f38095b
PhaniOEPL <phanisandeep.ch@optimizedelectrotech.com>
2025-05-23 12:17:02 +0530

    Update hailo_nms_decode.hpp print statementd

--- core/hailo/libs/postprocesses/detection/hailo_nms_decode.hpp
+++ core/hailo/libs/postprocesses/detection/hailo_nms_decode.hpp
@@ -142,11 +142,11 @@ private:
 			}
 		}
 		g_yolo_shmp->_numObjects=g_objCounter;
-	    fprintf("hailo detection x : %d", g_yolo_shmp->_detections[g_objCounter].tlx);
-	    fprintf("hailo detection x : %d", g_yolo_shmp->_detections[g_objCounter].tly);
-	    fprintf("hailo detection width : %d", g_yolo_shmp->_detections[g_objCounter].width);
-	    fprintf("hailo detection height : %d", g_yolo_shmp->_detections[g_objCounter].height);
-	    fprintf("hailo detection classID : %d", g_yolo_shmp->_detections[g_objCounter].classID);
+	    printf("hailo detection x : %d", g_yolo_shmp->_detections[g_objCounter].tlx);
+	    printf("hailo detection x : %d", g_yolo_shmp->_detections[g_objCounter].tly);
+	    sprintf("hailo detection width : %d", g_yolo_shmp->_detections[g_objCounter].width);
+	    sprintf("hailo detection height : %d", g_yolo_shmp->_detections[g_objCounter].height);
+	    printf("hailo detection classID : %d", g_yolo_shmp->_detections[g_objCounter].classID);
 
     }
 
