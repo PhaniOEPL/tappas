9096adc40afc7df7de3c95fce6285453cd2e0841
PhaniOEPL <phanisandeep.ch@optimizedelectrotech.com>
2025-05-23 12:28:40 +0530

    Update jde_tracker_update.hpp

--- core/hailo/tracking/jde_tracker/jde_tracker_update.hpp
+++ core/hailo/tracking/jde_tracker/jde_tracker_update.hpp
@@ -440,8 +440,13 @@ inline std::vector<STrack> JDETracker::update(std::vector<HailoDetectionPtr> &in
 		m_track_shmp->_tracks[i].height    = xyah[3]*m_track_shmp->_model_input_size_y; //h
         	m_track_shmp->_tracks[i].trackID   = temp_track.m_track_id;
 		m_track_shmp->_tracks[i].classtype = temp_track.m_class_id;
-		printf("INFO: TrackObjects: %d %d %d %d %d",m_track_shmp->_tracks[i].cX, m_track_shmp->_tracks[i].cY, 
-			m_track_shmp->_tracks[i].width, m_track_shmp->_tracks[i].height, m_track_shmp->_tracks[i].trackID, m_track_shmp->_tracks[i].classType);
+		printf("Hailo: Tracks x: %d\n",m_track_shmp->_tracks[i].cX);
+		printf("Hailo: Tracks y: %d\n",m_track_shmp->_tracks[i].cY);
+		printf("Hailo: Tracks width: %d\n",m_track_shmp->_tracks[i].width);
+		printf("Hailo: Tracks height: %d\n",m_track_shmp->_tracks[i].height);
+		printf("Hailo: Tracks track id: %d\n",m_track_shmp->_tracks[i].trackID);
+		printf("Hailo: Tracks classtype: %d\n",m_track_shmp->_tracks[i].classType);
+		
 	}		
      }
     return output_stracks;
