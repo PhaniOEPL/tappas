353ba238a56df9c5bc927b821ad1cb4401735994
PhaniOEPL <phanisandeep.ch@optimizedelectrotech.com>
2025-06-16 14:30:57 +0530

    Update hailo_nms_decode.hpp

--- core/hailo/libs/postprocesses/detection/hailo_nms_decode.hpp
+++ core/hailo/libs/postprocesses/detection/hailo_nms_decode.hpp
@@ -13,7 +13,7 @@
 #include "common/labels/coco_ninety.hpp"
 #include "common/labels/coco_visdrone.hpp"
 #include "common/labels/nv_imx.hpp"
-
+#include "common/labels/nv_imx5.hpp"
 //shared mem include
 #include<stdio.h>
 #include<sys/ipc.h>
