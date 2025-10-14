ea0471788a09c8772eea609ca2a957a8e7a8c5f3
PhaniOEPL <phanisandeep.ch@optimizedelectrotech.com>
2025-10-14 10:48:37 +0530

    Update hailo_nms_decode.hpp
    removed scaling 640/512

--- core/hailo/libs/postprocesses/detection/hailo_nms_decode.hpp
+++ core/hailo/libs/postprocesses/detection/hailo_nms_decode.hpp
@@ -147,11 +147,11 @@ private:
 			{
 				if(_objects.size() < DEFAULT_MAX_BOXES)
 				{
-                		_objects.push_back(HailoDetection(HailoBBox(dequant_bbox.x_min, dequant_bbox.y_min*640/512, w, h*640/512), class_index, labels_dict[class_index], confidence));
+                		_objects.push_back(HailoDetection(HailoBBox(dequant_bbox.x_min, dequant_bbox.y_min, w, h), class_index, labels_dict[class_index], confidence));
 				g_yolo_shmp->_detections[g_objCounter].tlx    = (int)(dequant_bbox.x_min*g_yolo_shmp->model_input_size_x);
-				g_yolo_shmp->_detections[g_objCounter].tly    = (int)(dequant_bbox.y_min*g_yolo_shmp->model_input_size_y*640/512);
+				g_yolo_shmp->_detections[g_objCounter].tly    = (int)(dequant_bbox.y_min*g_yolo_shmp->model_input_size_y);
 				g_yolo_shmp->_detections[g_objCounter].width  = (int)(w*g_yolo_shmp->model_input_size_x);
-				g_yolo_shmp->_detections[g_objCounter].height = (int)(h*g_yolo_shmp->model_input_size_y*640/512);
+				g_yolo_shmp->_detections[g_objCounter].height = (int)(h*g_yolo_shmp->model_input_size_y);
 				g_yolo_shmp->_detections[g_objCounter].classID= (int)(class_index);
 				g_objCounter++;
 				}
@@ -163,11 +163,11 @@ private:
  			{
 				if(_objects.size() < DEFAULT_MAX_BOXES)
 				{
-                		_objects.push_back(HailoDetection(HailoBBox(dequant_bbox.x_min, dequant_bbox.y_min*640/512, w, h*640/512), class_index, labels_dict[class_index], confidence));
+                		_objects.push_back(HailoDetection(HailoBBox(dequant_bbox.x_min, dequant_bbox.y_min, w, h), class_index, labels_dict[class_index], confidence));
 				g_yolo_shmp->_detections[g_objCounter].tlx    = (int)(dequant_bbox.x_min*g_yolo_shmp->model_input_size_x);
-				g_yolo_shmp->_detections[g_objCounter].tly    = (int)(dequant_bbox.y_min*g_yolo_shmp->model_input_size_y*640/512);
+				g_yolo_shmp->_detections[g_objCounter].tly    = (int)(dequant_bbox.y_min*g_yolo_shmp->model_input_size_y);
 				g_yolo_shmp->_detections[g_objCounter].width  = (int)(w*g_yolo_shmp->model_input_size_x);
-				g_yolo_shmp->_detections[g_objCounter].height = (int)(h*g_yolo_shmp->model_input_size_y*640/512);
+				g_yolo_shmp->_detections[g_objCounter].height = (int)(h*g_yolo_shmp->model_input_size_y);
 				g_yolo_shmp->_detections[g_objCounter].classID= (int)(class_index);
 				g_objCounter++;
 				}
@@ -179,11 +179,11 @@ private:
 			{
 				if(_objects.size() < DEFAULT_MAX_BOXES)
 				{
-                		_objects.push_back(HailoDetection(HailoBBox(dequant_bbox.x_min, dequant_bbox.y_min*640/512, w, h*640/512), class_index, labels_dict[class_index], confidence));
+                		_objects.push_back(HailoDetection(HailoBBox(dequant_bbox.x_min, dequant_bbox.y_min, w, h), class_index, labels_dict[class_index], confidence));
 				g_yolo_shmp->_detections[g_objCounter].tlx    = (int)(dequant_bbox.x_min*g_yolo_shmp->model_input_size_x);
-				g_yolo_shmp->_detections[g_objCounter].tly    = (int)(dequant_bbox.y_min*g_yolo_shmp->model_input_size_y*640/512);
+				g_yolo_shmp->_detections[g_objCounter].tly    = (int)(dequant_bbox.y_min*g_yolo_shmp->model_input_size_y);
 				g_yolo_shmp->_detections[g_objCounter].width  = (int)(w*g_yolo_shmp->model_input_size_x);
-				g_yolo_shmp->_detections[g_objCounter].height = (int)(h*g_yolo_shmp->model_input_size_y*640/512);
+				g_yolo_shmp->_detections[g_objCounter].height = (int)(h*g_yolo_shmp->model_input_size_y);
 				g_yolo_shmp->_detections[g_objCounter].classID= (int)(class_index);
 				g_objCounter++;
 				}
