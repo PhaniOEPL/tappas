8a0a8d1d3f7704bad4359e43c738fd7c6a181ac6
PhaniOEPL <phanisandeep.ch@optimizedelectrotech.com>
2025-10-27 16:27:39 +0530

    Update YOLOv8S output layer string and tensor reference

--- core/hailo/libs/postprocesses/detection/yolo_hailortpp.cpp
+++ core/hailo/libs/postprocesses/detection/yolo_hailortpp.cpp
@@ -18,7 +18,7 @@
 static const std::string DEFAULT_YOLOV5S_OUTPUT_LAYER = "yolov5s_nv12/yolov5_nms_postprocess";
 static const std::string DEFAULT_YOLOV5M_OUTPUT_LAYER = "yolov5_nms_postprocess";
 static const std::string DEFAULT_YOLOV5M_VEHICLES_OUTPUT_LAYER = "yolov5m_vehicles/yolov5_nms_postprocess";
-static const std::string DEFAULT_YOLOV8S_OUTPUT_LAYER = "Vis8s_1280x7044_v8/yolov8_nms_postprocess";
+static const std::string DEFAULT_YOLOV8S_OUTPUT_LAYER = "yolov8s/yolov8_nms_postprocess";
 static const std::string DEFAULT_YOLOV8M_OUTPUT_LAYER = "yolov8m/yolov8_nms_postprocess";
 static const std::string DEFAULT_YOLOV5M_8M_OUTPUT_LAYER = "nms_postprocess";
 static const std::string DEFAULT_YOLOV8L_OUTPUT_LAYER = "yolov8l/yolov8_nms_postprocess";
@@ -156,7 +156,7 @@ void yolov8s(HailoROIPtr roi)
     {
         return;
     }
-    auto post = HailoNMSDecode(roi->get_tensor(DEFAULT_YOLOV8S_OUTPUT_LAYER), common::nv_imxDrone);
+    auto post = HailoNMSDecode(roi->get_tensor(DEFAULT_YOLOV8S_OUTPUT_LAYER), common::nv_imx);
     auto detections = post.decode<float32_t, common::hailo_bbox_float32_t>();
     hailo_common::add_detections(roi, detections);
 }
