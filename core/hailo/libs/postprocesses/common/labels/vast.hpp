2c53c56bf1e39df97670849d82b6c61688fcff6d
PhaniOEPL <phanisandeep.ch@optimizedelectrotech.com>
2026-04-17 11:50:54 +0530

    Define vast map for labeled objects
    Added a static map to associate labels with their corresponding IDs.

--- core/hailo/libs/postprocesses/common/labels/vast.hpp
+++ core/hailo/libs/postprocesses/common/labels/vast.hpp
@@ -1,1 +1,24 @@
+/**
+* Copyright (c) 2021-2022 Hailo Technologies Ltd. All rights reserved.
+* Distributed under the LGPL license (https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt)
+**/
+#pragma once
+#include <map>
+namespace common
+{
+    static std::map<uint8_t, std::string> vast = {
+        {0, "unlabeled"},
+        {1, "bus"},
+        {2, "car"},
+        {3, "fire_extinguisher"},
+        {4, "forklift"},
+        {5, "person"},
+        {6, "safety_helmet"},
+        {7, "safety_jacket"},
+        {8, "towed_trolley"},
+        {9, "tractor"},
+        {10, "truck"}};
+}
+
+
 
