3d898bf46181a286b654a7123f9d82da29cddc95
PhaniOEPL <phanisandeep.ch@optimizedelectrotech.com>
2026-04-24 19:02:15 +0530

    Update init function signature in custom_yolo4_postprocess

--- core/hailo/libs/postprocesses/detection/custom_yolo4_postprocess.hpp
+++ core/hailo/libs/postprocesses/detection/custom_yolo4_postprocess.hpp
@@ -4,6 +4,6 @@
 
 __BEGIN_DECLS
 void custom_yolov4(HailoROIPtr roi, void *params_void_ptr);
-extern "C" void *init(const char *config_path, const char *func_name);
+extern "C" void *init(const char *config_path);
 void filter(HailoROIPtr roi);
 __END_DECLS
