1b0087ce77f6bfe19177b0ff856fc6da093dbb76
PhaniOEPL <phanisandeep.ch@optimizedelectrotech.com>
2026-04-24 19:38:06 +0530

    Change init function parameter type to std::string

--- core/hailo/libs/postprocesses/detection/custom_yolo4_postprocess.hpp
+++ core/hailo/libs/postprocesses/detection/custom_yolo4_postprocess.hpp
@@ -4,6 +4,6 @@
 
 __BEGIN_DECLS
 void custom_yolov4(HailoROIPtr roi, void *params_void_ptr);
-extern "C" void *init(const char *config_path);
+extern "C" void *init(const std::string config_path);
 void filter(HailoROIPtr roi);
 __END_DECLS
