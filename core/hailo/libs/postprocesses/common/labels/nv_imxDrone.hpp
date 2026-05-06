7abee6e64c73771d8b2ccd2f5229e0abf02cce89
PhaniOEPL <phanisandeep.ch@optimizedelectrotech.com>
2026-05-06 12:39:09 +0530

    added drone label

---
+++ core/hailo/libs/postprocesses/common/labels/nv_imxDrone.hpp
@@ -0,0 +1,12 @@
+/**
+* Copyright (c) 2021-2022 Hailo Technologies Ltd. All rights reserved.
+* Distributed under the LGPL license (https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt)
+**/
+#pragma once
+#include <map>
+namespace common
+{
+    static std::map<uint8_t, std::string> nv_imxDrone = {
+        {0, "unlabeled"},
+        {1, "drones"}};
+}
