cab8b29915812a45ca4bcd628f9893fefdeb1315
PhaniOEPL <phanisandeep.ch@optimizedelectrotech.com>
2026-06-16 18:58:48 +0530

    Fix output layer string for YOLOv9

--- core/hailo/libs/postprocesses/detection/yolo_hailortpp.cpp
+++ core/hailo/libs/postprocesses/detection/yolo_hailortpp.cpp
@@ -23,7 +23,7 @@ static const std::string DEFAULT_YOLOV8M_OUTPUT_LAYER = "yolov8m/yolov8_nms_post
 static const std::string DEFAULT_YOLOV5M_8M_OUTPUT_LAYER = "nms_postprocess";
 static const std::string DEFAULT_YOLOV8L_OUTPUT_LAYER = "yolov8l/yolov8_nms_postprocess";
 static const std::string DEFAULT_YOLOV8_OUTPUT_LAYER = "yolo_v8/yolov8_nms_postprocess";
-static const std::string DEFAULT_YOLOV9_OUTPUT_LAYER = "yolov9/yolov8_nms_postprocess";
+static const std::string DEFAULT_YOLOV9_OUTPUT_LAYER = "yolov9c/yolov8_nms_postprocess";
 
 #if __GNUC__ > 8
 #include <filesystem>
